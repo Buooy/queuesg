b3de41e7c48d0986e5412f055cfa0400cf724f71
aaron <aaron.lee@buooy.com>
2021-01-30 22:52:32 +0800

    added move card from list to list action

--- .netlify/functions/trello.js
+++ .netlify/functions/trello.js
@@ -1,7 +1,26 @@
+const { endsWith } = require('*.jpg')
+const actionMoveCardFromListToList = require('./trello/actionMoveCardFromListToList')
+
 exports.handler = async (event, context) => {
   if (event.body) {
     const data = JSON.parse(event.body)
     console.log('data', data)
+
+    if (
+      data.action &&
+      data.action.display &&
+      data.action.display.translationKey
+    ) {
+      switch(data.action.display.translationKey) {
+        case 'action_move_card_from_list_to_list':
+          console.log(JSON.stringify(data.action))
+          break;
+
+        default:
+          console.log('Info: Uncaught Action')
+          console.log(JSON.stringify(data))
+      }
+    }
   } else {
     console.log('no event body')
   }
---
+++ .netlify/functions/trello/actionMoveCardFromListToList.js
@@ -0,0 +1,8 @@
+/**
+ * Webhook for trello when a card moves from list to list
+ */
+const actionMoveCardFromListToList = () => {
+
+}
+
+module.exports = actionMoveCardFromListToList
