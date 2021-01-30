17106fdf006ce7a5036179bfcc821978b5fc28ff
aaron <aaron.lee@buooy.com>
2021-01-30 22:31:41 +0800

    Update trello.js

--- .netlify/functions/trello.js
+++ .netlify/functions/trello.js
@@ -1,4 +1,6 @@
 exports.handler = async (event, context) => {
+  console.log('event', event.body)
+
   if (event.body !== null) {
     const data = JSON.parse(event.body)
     console.log('data', data)
